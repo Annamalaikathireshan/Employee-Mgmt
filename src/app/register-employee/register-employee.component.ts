d5990a13c01d616530a14aa328c01003bf32ab08
annamalai kathireshan <annamalaikathiresan@gmail.com>
2024-02-17 20:52:20 +0530

    adding validation

--- src/app/register-employee/register-employee.component.ts
+++ src/app/register-employee/register-employee.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup,Validators } from '@angular/forms';
-import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
+// import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
 
 
 
@@ -13,19 +13,26 @@ export class RegisterEmployeeComponent implements OnInit {
  registerForm!: FormGroup; // Define employeeForm as a FormGroup
   //title:'Employee-app' | undefined;
   submitted = false;
+  jobTitle = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]);
+  employeeId = new FormControl('',[Validators.required, Validators.pattern('[0-9]{8}')]);
+  contactNumber = new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]);
+  department = new FormControl('',Validators.required);
+  salary = new FormControl('',[Validators.required, Validators.min(0)]);
+  email = new FormControl('',[Validators.required, Validators.email]);
+  gender = new FormControl('',[Validators.required]);
 
   constructor(private formBuilder: FormBuilder) { }
 
 
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-      employeeId: ['', [Validators.required, Validators.pattern('[0-9]{8}')]],
+      employeeId1: ['', [Validators.required, Validators.pattern('[0-9]{8}')]],
       dob: ['', Validators.required],
       gender: ['', Validators.required],
-      contactNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      contactNumber1: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
       email: ['', [Validators.required, Validators.email]],
       address: ['', Validators.required],
-      jobTitle: ['', Validators.required],
+      jobTitle1: ['', Validators.required],
       department: ['', Validators.required],
       salary: ['', [Validators.required, Validators.min(0)]],
       hireDate: ['', Validators.required]
@@ -42,8 +49,8 @@ export class RegisterEmployeeComponent implements OnInit {
   }*/
 
   registerSubmitted(){
-    console.log(this.registerForm.get('employeeId'));
-    this.submitted=true;
+    // this.submitted=true;
+    console.log(this.email.errors);
 
   }
 }
